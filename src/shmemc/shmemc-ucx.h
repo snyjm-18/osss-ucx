b56754a20b1bff117dddb6435735db777dba6808
Jack <snyjm-18@rhodes.edu>
2018-06-01 11:13:52 -0600

    fixing compilation issues

--- src/shmemc/shmemc-ucx.h
+++ src/shmemc/shmemc-ucx.h
@@ -1,12 +1,13 @@
 /* For license: see LICENSE file at top-level */
 
+
 #ifndef _SHMEMC_UCX_H
 #define _SHMEMC_UCX_H 1
 
 void shmemc_ucx_init(void);
 void shmemc_ucx_finalize(void);
 
-void shmemc_ucx_init_am(shmem_ctx_t ctx);
+void shmemc_ucx_init_am();
 
 void shmemc_ucx_make_remote_endpoints(void);
 
